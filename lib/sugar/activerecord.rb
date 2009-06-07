c567b250b96ec10612bef70ed12c4cd5258a054d
Alexander Semyonov <rotuka@rotuka.com>
2009-06-07 06:16:46 +0400

    Added sluggy activerecord mechanismes

---
+++ lib/sugar/activerecord.rb
@@ -0,0 +1,11 @@
+module Sugar
+  module Activerecord
+    def slug
+      read_attribute(:slug) || to_s && write_attribute(:slug, to_s.parameterize) && read_attribute(:slug)
+    end
+
+    def to_param
+      slug.present? ? "#{id}-#{slug}" : id.to_s
+    end
+  end
+end
