294a5833492fb2868d11d2f1da05817b8bd3f4ba
Alexander Semyonov <rotuka@rotuka.com>
2009-09-01 07:44:41 +0400

    added default l10ns

--- lib/sugar.rb
+++ lib/sugar.rb
@@ -1,4 +1,7 @@
 module Sugar
+  def self.locale_files
+    Dir[File.join(File.dirname(__FILE__), 'sugar', 'locales', '*')]
+  end
 end
 
 require 'sugar/actionview'
@@ -7,4 +10,5 @@ require 'sugar/activerecord'
 if defined? Rails
   ActionView::Base.send :include, Sugar::Actionview if defined? ActionView
   ActionController::Base.send :include, Sugar::Actioncontroller if defined? ActionController
+  I18n.load_path.unshift(*Sugar.locale_files)
 end
